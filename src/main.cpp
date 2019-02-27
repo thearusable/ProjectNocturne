7457f8591848babeeecf0a3f5f40cce0c9947664
Arkadiusz Szczepkowicz <arek.szczepkowicz@gmail.com>
2019-02-27 19:57:49 +0100

    add clang format

--- src/main.cpp
+++ src/main.cpp
@@ -33,124 +33,131 @@ Create and destroy a Vulkan surface on an SDL window.
 
 // Tell SDL not to mess with main()
 #define SDL_MAIN_HANDLED
-#include <glm/glm.hpp>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_syswm.h>
 #include <SDL2/SDL_vulkan.h>
-#include <vulkan/vulkan.hpp>
-
+#include <glm/glm.hpp>
 #include <iostream>
 #include <vector>
+#include <vulkan/vulkan.hpp>
 
-int main()
+int
+main()
 {
-    // Create an SDL window that supports Vulkan rendering.
-    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cout << "Could not initialize SDL." << std::endl;
-        return 1;
-    }
-    SDL_Window* window = SDL_CreateWindow("Vulkan Window", SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_VULKAN);
-    if(window == NULL) {
-        std::cout << "Could not create SDL window." << std::endl;
-        return 1;
-    }
-    
-    // Get WSI extensions from SDL (we can add more if we like - we just can't remove these)
-    unsigned extension_count;
-    if(!SDL_Vulkan_GetInstanceExtensions(window, &extension_count, NULL)) {
-        std::cout << "Could not get the number of required instance extensions from SDL." << std::endl;
-        return 1;
-    }
-    std::vector<const char*> extensions(extension_count);
-    if(!SDL_Vulkan_GetInstanceExtensions(window, &extension_count, extensions.data())) {
-        std::cout << "Could not get the names of required instance extensions from SDL." << std::endl;
-        return 1;
-    }
-
-    // Use validation layers if this is a debug build
-    std::vector<const char*> layers;
-#if defined(_DEBUG)
-    layers.push_back("VK_LAYER_LUNARG_standard_validation");
-#endif
-
-    // vk::ApplicationInfo allows the programmer to specifiy some basic information about the
-    // program, which can be useful for layers and tools to provide more debug information.
-    vk::ApplicationInfo appInfo = vk::ApplicationInfo()
-        .setPApplicationName("Vulkan C++ Windowed Program Template")
-        .setApplicationVersion(1)
-        .setPEngineName("LunarG SDK")
-        .setEngineVersion(1)
-        .setApiVersion(VK_API_VERSION_1_0);
-
-    // vk::InstanceCreateInfo is where the programmer specifies the layers and/or extensions that
-    // are needed.
-    vk::InstanceCreateInfo instInfo = vk::InstanceCreateInfo()
-        .setFlags(vk::InstanceCreateFlags())
-        .setPApplicationInfo(&appInfo)
-        .setEnabledExtensionCount(static_cast<uint32_t>(extensions.size()))
-        .setPpEnabledExtensionNames(extensions.data())
-        .setEnabledLayerCount(static_cast<uint32_t>(layers.size()))
-        .setPpEnabledLayerNames(layers.data());
-
-    // Create the Vulkan instance.
-    vk::Instance instance;
-    try {
-        instance = vk::createInstance(instInfo);
-    } catch(const std::exception& e) {
-        std::cout << "Could not create a Vulkan instance: " << e.what() << std::endl;
-        return 1;
-    }
-
-    // Create a Vulkan surface for rendering
-    VkSurfaceKHR c_surface;
-    if(!SDL_Vulkan_CreateSurface(window, static_cast<VkInstance>(instance), &c_surface)) {
-        std::cout << "Could not create a Vulkan surface." << std::endl;
-        return 1;
-    }
-    vk::SurfaceKHR surface(c_surface);
-
-    // This is where most initializtion for a program should be performed
-
-	uint32_t devices_count;
-    VkPhysicalDevice devices;
-
-	VkResult result = vkEnumeratePhysicalDevices(instance, &devices_count, NULL);
-    if (result != VkResult::VK_SUCCESS)
+	// Create an SDL window that supports Vulkan rendering.
+	if(SDL_Init(SDL_INIT_VIDEO) != 0)
 	{
-          std::cout << "Could not list physical devices." << std::endl;
-          return 1;
+		std::cout << "Could not initialize SDL." << std::endl;
+		return 1;
+	}
+	SDL_Window* window = SDL_CreateWindow("Vulkan Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_VULKAN);
+	if(window == NULL)
+	{
+		std::cout << "Could not create SDL window." << std::endl;
+		return 1;
 	}
 
-	
+	// Get WSI extensions from SDL (we can add more if we like - we just can't
+	// remove these)
+	unsigned extension_count;
+	if(!SDL_Vulkan_GetInstanceExtensions(window, &extension_count, NULL))
+	{
+		std::cout << "Could not get the number of required instance extensions from SDL." << std::endl;
+		return 1;
+	}
+	std::vector<const char*> extensions(extension_count);
+	if(!SDL_Vulkan_GetInstanceExtensions(window, &extension_count, extensions.data()))
+	{
+		std::cout << "Could not get the names of required instance extensions from SDL." << std::endl;
+		return 1;
+	}
 
-    // Poll for user input.
-    bool stillRunning = true;
-    while(stillRunning) {
+	// Use validation layers if this is a debug build
+	std::vector<const char*> layers;
+#if defined(_DEBUG)
+	layers.push_back("VK_LAYER_LUNARG_standard_validation");
+#endif
 
-        SDL_Event event;
-        while(SDL_PollEvent(&event)) {
+	// vk::ApplicationInfo allows the programmer to specifiy some basic
+	// information about the program, which can be useful for layers and tools to
+	// provide more debug information.
+	vk::ApplicationInfo appInfo = vk::ApplicationInfo()
+	                                 .setPApplicationName("Vulkan C++ Windowed Program Template")
+	                                 .setApplicationVersion(1)
+	                                 .setPEngineName("LunarG SDK")
+	                                 .setEngineVersion(1)
+	                                 .setApiVersion(VK_API_VERSION_1_0);
 
-            switch(event.type) {
+	// vk::InstanceCreateInfo is where the programmer specifies the layers and/or
+	// extensions that are needed.
+	vk::InstanceCreateInfo instInfo = vk::InstanceCreateInfo()
+	                                     .setFlags(vk::InstanceCreateFlags())
+	                                     .setPApplicationInfo(&appInfo)
+	                                     .setEnabledExtensionCount(static_cast<uint32_t>(extensions.size()))
+	                                     .setPpEnabledExtensionNames(extensions.data())
+	                                     .setEnabledLayerCount(static_cast<uint32_t>(layers.size()))
+	                                     .setPpEnabledLayerNames(layers.data());
 
-            case SDL_QUIT:
-                stillRunning = false;
-                break;
+	// Create the Vulkan instance.
+	vk::Instance instance;
+	try
+	{
+		instance = vk::createInstance(instInfo);
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "Could not create a Vulkan instance: " << e.what() << std::endl;
+		return 1;
+	}
 
-            default:
-                // Do nothing.
-                break;
-            }
-        }
+	// Create a Vulkan surface for rendering
+	VkSurfaceKHR c_surface;
+	if(!SDL_Vulkan_CreateSurface(window, static_cast<VkInstance>(instance), &c_surface))
+	{
+		std::cout << "Could not create a Vulkan surface." << std::endl;
+		return 1;
+	}
+	vk::SurfaceKHR surface(c_surface);
 
-        SDL_Delay(10);
-    }
+	// This is where most initializtion for a program should be performed
 
-    // Clean up.
-    instance.destroySurfaceKHR(surface);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    instance.destroy();
+	uint32_t         devices_count;
+	VkPhysicalDevice devices;
 
-    return 0;
+	VkResult result = vkEnumeratePhysicalDevices(instance, &devices_count, NULL);
+	if(result != VkResult::VK_SUCCESS)
+	{
+		std::cout << "Could not list physical devices." << std::endl;
+		return 1;
+	}
+
+	// Poll for user input.
+	bool stillRunning = true;
+	while(stillRunning)
+	{
+		SDL_Event event;
+		while(SDL_PollEvent(&event))
+		{
+			switch(event.type)
+			{
+				case SDL_QUIT:
+					stillRunning = false;
+					break;
+
+				default:
+					// Do nothing.
+					break;
+			}
+		}
+
+		SDL_Delay(10);
+	}
+
+	// Clean up.
+	instance.destroySurfaceKHR(surface);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	instance.destroy();
+
+	return 0;
 }
