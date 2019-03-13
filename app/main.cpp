b64c6afdedb933fa857f1e59dace50195653d310
Jacob Domagala <domagala.jacob@gmail.com>
2019-03-13 11:56:15 +0100

    Initial folder and cmake setup

---
+++ app/main.cpp
@@ -0,0 +1,9 @@
+#include <Window.hpp>
+
+#undef main
+
+int
+main(int argc, char* argv[])
+{
+
+}
---
+++ src/Window/Window.hpp
@@ -0,0 +1,15 @@
+#ifndef NOC_WINDOW_WINDOW_HPP
+#define NOC_WINDOW_WINDOW_HPP
+
+#include<SDL.h>
+
+namespace noc::Window{
+
+class Window
+{
+   Window(uint64_t width, uint64_t height);
+};
+
+}
+
+#endif // NOC_WINDOW_WINDOW_HPP
