8944fbe9d82027ebd0ddfe56ba21c7d29141775b
Jacob Domagala <domagala.jacob@gmail.com>
2019-03-14 00:35:22 +0100

    Add skeleton code for Window class

--- app/main.cpp
+++ app/main.cpp
@@ -5,5 +5,12 @@
 int
 main(int argc, char* argv[])
 {
+	noc::Window::Window window(1024, 768, "Editor");
 
+	while (1)
+	{
+		// MAIN LOOP
+	}
+
+	return 0;
 }
--- src/Window/Window.hpp
+++ src/Window/Window.hpp
@@ -1,15 +1,24 @@
 #ifndef NOC_WINDOW_WINDOW_HPP
 #define NOC_WINDOW_WINDOW_HPP
 
-#include<SDL.h>
+#include <SDL.h>
+#include <memory>
+#include <string>
 
-namespace noc::Window{
-
+namespace noc::Window
+{
 class Window
 {
-   Window(uint64_t width, uint64_t height);
+public:
+	Window(uint32_t width, uint32_t height, const std::string& title);
+
+private:
+	uint32_t    m_width;
+	uint32_t    m_height;
+	SDL_Window* m_pWindow;
+	std::string m_title;
 };
 
-}
+}   // namespace noc::Window
 
-#endif // NOC_WINDOW_WINDOW_HPP
+#endif   // NOC_WINDOW_WINDOW_HPP
